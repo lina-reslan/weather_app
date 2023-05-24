e8bcc14bf508640c31baae8eee9bd2f736dcf386
lina.raslan <lina.raslan@infostrategic.com>
2023-05-24 10:59:37 +0300

    weather app files

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,53 @@
+
+import 'package:clima/services/weather.dart';
+import 'package:flutter/material.dart';
+
+
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import 'location_screen.dart';
+
+
+const spinkit = SpinKitRotatingCircle(
+  color: Colors.white,
+  size: 50.0,
+);
+
+
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState(){
+    super.initState();
+    getLocationData();
+  }
+
+  Future<void> getLocationData() async {
+    WeatherModel weatherModel = WeatherModel() ;
+    var weatherData= await weatherModel.getLocationWeather();
+    print(weatherData);
+
+      Navigator.push(context,MaterialPageRoute(builder: (context) {
+        return LocationScreen(
+          locationWeather:weatherData,
+        );
+      }));
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: spinkit,
+      ),
+    );
+  }
+}
---
+++ lib/screens/location_screen.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:clima/utilities/constants.dart';
+import 'package:clima/services/weather.dart';
+import 'package:clima/screens/city_screen.dart';
+
+class LocationScreen extends StatefulWidget {
+  LocationScreen({this.locationWeather});
+  final dynamic  locationWeather;
+  @override
+  _LocationScreenState createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+  WeatherModel weather = WeatherModel();
+  int temperature;
+  String weatherIcon;
+  String cityName;
+  String message;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    updateData(widget.locationWeather);
+  }
+
+  void updateData(dynamic locationWeather) {
+    setState(() {
+      if (locationWeather == null) {
+        temperature = 0;
+        weatherIcon = 'Error';
+        message = 'unable to get weather data';
+        cityName = ' ';
+        return;
+      }
+      double temp = locationWeather['main']['temp'];
+      temperature = temp.toInt();
+      message = weather.getMessage(temperature);
+      var condition = locationWeather['weather'][0]['id'];
+      weatherIcon = weather.getWeatherIcon(condition);
+      cityName = locationWeather['name'];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/location_background.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.white.withOpacity(0.8), BlendMode.dstATop),
+          ),
+        ),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  TextButton(
+                    onPressed: () async {
+                      var weatherData = await weather.getLocationWeather();
+                      updateData(weatherData);
+                    },
+                    child: Icon(
+                      Icons.near_me,
+                      size: 50.0,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                    var typeName = await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return CityScreen();
+                          },
+                        ),
+                      );
+                    if(typeName != null){
+                          var weatherData =await weather.getCityWeather(typeName);
+                          print(weatherData);
+                          updateData(weatherData);
+                          }
+                    },
+                    child: Icon(
+                      Icons.location_city,
+                      size: 50.0,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 15.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      '$temperature°',
+                      style: kTempTextStyle,
+                    ),
+                    Text(
+                      weatherIcon,
+                      style: kConditionTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 15.0),
+                child: Text(
+                  '$message in $cityName',
+                  textAlign: TextAlign.right,
+                  style: kMessageTextStyle,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
